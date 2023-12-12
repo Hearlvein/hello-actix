06ccaf1f8e2f49349e56f238e23e1b0ca7cb87d3
Hearlvein <james.chaintron@gmail.com>
2023-12-12 23:35:09 +0100

    Modifier POST /echo

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,24 @@ async fn hello() -> impl Responder {
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
+    // Concatenate "start", request body and "end" in a new String
+    let req_body = format!("start{}end", req_body);
     HttpResponse::Ok().body(req_body)
 }
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
